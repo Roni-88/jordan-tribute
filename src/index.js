f144358012d21cf7a4de163b79f5876b17e9af62
Roni 88 <ronix88x@hotmail.com>
2023-03-23 19:43:59 -0300

    ajustes gerais

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 const botoesCarrossel = document.querySelectorAll ('.botao')
+const imagens = document.querySelectorAll ('.imagem')
 
-botoesCarrossel.forEach ((botao) => {
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado')
+    botaoSelecionado.classList.remove('selecionado')
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa')
+    imagemAtiva.classList.remove('ativa')
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado')
+}
+
+function selecionarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa')
+}
+
+botoesCarrossel.forEach ((botao, indice, array) => {
     botao.addEventListener ('click', () => {
-        const botaoSelecionado = document.querySelector ('.selecionado')
-        botaoSelecionado.classList.remove ('selecionado')
-        botao.classList.add ('selecionado')
-    })
-})
-
-const imagensCarrossel = document.querySelectorAll ('.imagem')
-
-imagensCarrossel.forEach ((imagem) => {
-    imagem.addEventListener ('click', () => {
-        const imagemAtiva = document.querySelector ('.ativa')
-        imagemAtiva.classList.remove ('ativa')
-        imagem.classList.add ('ativa')
+        desativarBotaoSelecionado()
+        selecionarBotaoCarrossel(botao)
+        esconderImagemAtiva()
+        selecionarImagemDeFundo(indice)
     })
 })

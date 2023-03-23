f37570393c80fdef4a65f25b70379ae14185571d
Roni 88 <ronix88x@hotmail.com>
2023-03-23 17:14:15 -0300

    ajustes gerais

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-1920L por 1080A
+const botoesCarrossel = document.querySelectorAll ('.botao')
+
+botoesCarrossel.forEach ((botao) => {
+    botao.addEventListener ('click', () => {
+        const botaoSelecionado = document.querySelector ('.selecionado')
+        botaoSelecionado.classList.remove ('selecionado')
+        botao.classList.add ('selecionado')
+    })
+})
+
+const imagensCarrossel = document.querySelectorAll ('.imagem')
+
+imagensCarrossel.forEach ((imagem) => {
+    imagem.addEventListener ('click', () => {
+        const imagemAtiva = document.querySelector ('.ativa')
+        imagemAtiva.classList.remove ('ativa')
+        imagem.classList.add ('ativa')
+    })
+})
